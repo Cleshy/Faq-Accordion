1fe01e40ffb0df8d49040c3f3710f640591d40c2
Czipa Szilárd <szilard.czipa@gmail.com>
2024-01-09 06:34:12 +0100

    Initial commit

---
+++ src/containers/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Card from "./Card";
+import "./App.css";
+
+function App() {
+  return (
+    <Card>
+      <h1>Hello World</h1>
+    </Card>
+  );
+}
+
+export default App;
